5d083e47539366ce677884d748cbc5b6b50952c8
Mahmou-d6356567377 <ma2327424@gmail.com>
2024-04-09 10:02:13 +0200

    create notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const NoteApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return  Container();
   }
 }
