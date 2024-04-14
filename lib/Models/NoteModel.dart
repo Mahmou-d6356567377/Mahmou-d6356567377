a346658a5ab35f4934f903d21adbddbdd47c184f
Mahmou-d6356567377 <131625133+Mahmou-d6356567377@users.noreply.github.com>
2024-04-14 16:42:56 +0200

    create type adabtor for note

--- lib/Models/NoteModel.dart
+++ lib/Models/NoteModel.dart
@@ -1,9 +1,17 @@
 
-class NoteModel{
-  final String title;
-  final String content;
+import 'package:hive/hive.dart';
 
+  part 'NoteModel.g.dart';
+@HiveType(typeId: 0)
+
+class NoteModel extends HiveObject{
+  @HiveField(0)
+  final String title;
+    @HiveField(1)
+ final String content;
+  @HiveField(3)
   final String date;
+    @HiveField(4)
   final int color;
 
   NoteModel({required this.title, required this.content, required this.date, required this.color});
