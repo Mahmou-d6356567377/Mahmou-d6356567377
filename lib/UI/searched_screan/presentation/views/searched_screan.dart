b974b8bdd9e64dfbfda204d8300d7a68b1b97b63
Mahmou-d6356567377 <131625133+Mahmou-d6356567377@users.noreply.github.com>
2024-07-06 23:13:29 +0300

    finish searched screan UI

---
+++ lib/UI/searched_screan/presentation/views/searched_screan.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mybook/UI/home_screan/presentation/View/widgets/newest_book_list.dart';
+import 'package:mybook/utils/constants.dart';
+
+class SearchedScrean extends StatelessWidget {
+  const SearchedScrean({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor:  const Color.fromARGB(255, 21, 61, 86),
+            //floating: true,
+            expandedHeight: 180,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(KlogoImage),
+            ),
+          
+          ),
+         const  SliverToBoxAdapter(child: NewestBookList(isScroll: true),)
+        ],
+      ),
+    );
+  }
+}
