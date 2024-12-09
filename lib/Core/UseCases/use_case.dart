a0c049a271cc7710f5d7d31c3665845d15d326a3
Mahmou-d6356567377 <131625133+Mahmou-d6356567377@users.noreply.github.com>
2024-12-09 15:47:04 +0200

    Delete lib directory

--- lib/Core/UseCases/use_case.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:mybook/Core/Error/failure.dart';
-
-abstract class UseCase<Type, Param> {
-// templete
-  Future<Either<Failure, Type>> call([Param param]);
-}
